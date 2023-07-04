80943bd1df37cff2585e4bbf3d42aab4f389b92f
Muhammad Taufiqurrohman <tareva863@gmail.com>
2023-07-04 15:37:28 +0700

    Signup User with Appwrite Auth

---
+++ lib/apis/auth_api.dart
@@ -0,0 +1,38 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:fpdart/fpdart.dart';
+
+import '../core/core.dart';
+
+abstract class IAuthAPI {
+  FutureEither<model.User> signUp({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthAPI implements IAuthAPI {
+  final Account _account;
+  AuthAPI({required Account account}) : _account = account;
+
+  @override
+  FutureEither<model.User> signUp(
+      {required String email, required String password}) async {
+    try {
+      final account = await _account.create(
+        userId: ID.unique(),
+        email: email,
+        password: password,
+      );
+      return right(account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+        Failure(e.message ?? 'Some unexpected error occured', stackTrace),
+      );
+    } catch (e, stackTrace) {
+      return left(
+        Failure(e.toString(), stackTrace),
+      );
+    }
+  }
+}
