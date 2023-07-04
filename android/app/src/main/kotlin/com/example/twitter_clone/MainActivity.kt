790a6f4cd1697e632d12d0c73049da4edfacc97c
Muhammad Taufiqurrohman <tareva863@gmail.com>
2023-07-04 14:46:29 +0700

    persiapan project

---
+++ android/app/src/main/kotlin/com/example/twitter_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.taufiq.twitter_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '6462f8ce54627b34dfad';
+  static const String projectId = '64293b6af0e9a554099d';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+}
