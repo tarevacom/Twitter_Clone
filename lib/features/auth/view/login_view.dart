7f31df20f2271945c63690b13600abcf9448b15d
Muhammad Taufiqurrohman <tareva863@gmail.com>
2023-07-04 15:17:52 +0700

    login ui

--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import '../../../../common/rounded_small_button.dart';
+import '../../../constants/ui_constants.dart';
+import '../widgets/auth_field.dart';
+import '../../../theme/theme.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -8,8 +13,62 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final appBar = UIConstants.appBar();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: appBar,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                AuthField(controller: emailController, hintText: 'Email'),
+                const SizedBox(height: 25.0),
+                AuthField(controller: passwordController, hintText: 'Password'),
+                const SizedBox(height: 40.0),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: RoundedSmallButton(
+                    onTap: () {},
+                    label: 'Done',
+                  ),
+                ),
+                const SizedBox(height: 40.0),
+                RichText(
+                  text: TextSpan(
+                    text: "Don't have an account?",
+                    style: const TextStyle(
+                      fontSize: 16,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: " Sign up",
+                        style: const TextStyle(
+                          color: Pallete.blueColor,
+                          fontSize: 16,
+                        ),
+                        recognizer: TapGestureRecognizer()..onTap = () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
